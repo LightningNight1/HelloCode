c56fbfd11f91ed14d8c4268815445344e97d1a10
LightningNight1 <zoebennnettt@gmail.com>
2022-09-14 20:43:47 +0300

    add Example 005 Condition Id-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "bob")
+{
+    Console.WriteLine("Hooray! It's a Bob!");
+}
+else
+{
+    Console.WriteLine("Hello,");
+    Console.WriteLine(username);
+}
